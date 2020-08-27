f7f478bd068fd03a9ede04adfd04e6ab9a310f18
Rob Mixon <mixonroberth@gmail.com>
2020-08-27 07:00:48 -0500

    first commit

---
+++ guessingGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace guessingGame {
+    class Program {
+        static void Main (string[] args) {
+
+            Console.WriteLine ("Guess the number that I am thinking from 0-10 and I will grant you one wish!");
+
+            int secretNumber = 42;
+            int guess = Convert.ToInt32 (Console.ReadLine ());
+
+            if (guess == secretNumber) {
+                Console.WriteLine ("Impossible! How did you know?");
+            } else {
+                Console.WriteLine ("Bwahahah, incorrect! Guess again");
+            }
+        }
+    }
+}
